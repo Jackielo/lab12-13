32c24c3312316eaefbdb3e91b0b838f818fc953c
Jackie Lo <j_lo@outlook.com>
2015-11-23 16:38:36 +0000

    Update

--- lab11/test.py
+++ lab11/test.py
@@ -4,4 +4,5 @@ import boto
 req = urllib2.Request('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
 f = urllib2.urlopen(req)
 code = f.read()
-print(code[0:20] + "\n" + code[21:-1] + "\n" + boto.Version)
+access, key = code.split(":")
+print(access + "\n" + key + "\n" + boto.Version)
