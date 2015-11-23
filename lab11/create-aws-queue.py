bd58416621cda6c031c390fae8876c9d67fd6012
Jackie Lo <j_lo@outlook.com>
2015-11-23 16:49:14 +0000

    Added create queue

---
+++ lab11/create-aws-queue.py
@@ -0,0 +1,26 @@
+
+import boto.sqs
+import boto.sqs.queue
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+import sys
+import urllib2
+
+# Get the keys from a specific url and then use them to connect to AWS Service
+req = urllib2.Request('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
+f = urllib2.urlopen(req)
+code = f.read()
+access, key = code.split(":")
+access_key_id = access
+secret_access_key = key
+
+print(access_key_id)
+print(secret_access_key)
+
+# Set up a connection to the AWS service.
+conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
+
+# Get a list of the queues that exists and then print the list out
+rs = conn.create_queue(sys.argv)
+
