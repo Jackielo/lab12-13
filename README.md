b5ac3460c8361a7e60d9a3e99db1eb20dcf18664
Jackie <j_lo@outlook.com>
2015-11-23 17:39:23 +0200

    Added file

--- README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-# DT211C-Cloud-Computing
-=======
-# lab11
->>>>>>> 7f35ff9b99ea6cb8fef2c1f510e60c3c8f8c259d
--- lab3/lab2II.py
+++
@@ -1,6 +0,0 @@
-word_input = input('Enter the word: ')
-word_input = word_input.lower()
-if(word_input == word_input[::-1]):
-  print("True")
-else:
-  print("False")
