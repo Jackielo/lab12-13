494dc8c75e9ee147ab5794bf07c9a35706b61a71
Jackie Lo <j_lo@outlook.com>
2015-11-30 15:26:10 +0000

    Update

--- lab11/delete-aws-queue.py
+++ lab11/delete-aws-queue.py
@@ -22,6 +22,6 @@ conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, 
 #rs = conn.delete_queue(sys.argv[1])
 rs = conn.get_all_queues()
 for q in rs:
-  if q.id == sys.argv[0]
+  if q.id == sys.argv[0]:
 	print q.id
   conn.delete_queue(q)
