e88c2659d25210ad3dab99689d4dc32458153aa1
Jackie Lo <j_lo@outlook.com>
2015-12-07 10:39:48 +0000

    Updated file

--- lab12-13/smfa.py
+++ lab12-13/smfa.py
@@ -13,16 +13,16 @@ def index():
 
 @app.route('/queues',	methods=['GET'])
 def	queues_index():
-"""
-List	all	queues
-curl	-s	-X	GET	-H	'Accept:	application/json'	http://localhost:5000/queues	|	python	-mjson.tool
-"""
-all	=	[]
-conn	=	get_conn()
-for	q	in	conn.get_all_queues():
-all.append(q.name)
-resp	=	json.dumps(all)
-return	Response(response=resp,	mimetype="application/json")
+  """
+  List	all	queues
+  curl	-s	-X	GET	-H	'Accept:	application/json'	http://localhost:5000/queues	|	python	-mjson.tool
+  """
+  all	=	[]
+  conn	=	get_conn()
+  for	q	in	conn.get_all_queues():
+  all.append(q.name)
+  resp	=	json.dumps(all)
+  return	Response(response=resp,	mimetype="application/json")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080, debug=True)
