c3fa70f88f41efa52a7c1b5362feb239c1f5c3a4
Jackie Lo <j_lo@outlook.com>
2015-12-07 10:37:23 +0000

    Updated file

--- lab12-13/smfa.py
+++ lab12-13/smfa.py
@@ -9,28 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return """
-  Available API endpoints:
-
-  GET /containers                     List all containers
-  GET /containers?state=running      List running containers (only)
-  GET /containers/<id>                Inspect a specific container
-  GET /containers/<id>/logs           Dump specific container logs
-  GET /images                         List all images
-
-
-  POST /images                        Create a new image
-  POST /containers                    Create a new container
-
-  PATCH /containers/<id>              Change a container's state
-  PATCH /images/<id>                  Change a specific image's attributes
-
-  DELETE /containers/<id>             Delete a specific container
-  DELETE /containers                  Delete all containers (including running)
-  DELETE /images/<id>                 Delete a specific image
-  DELETE /images                      Delete all images
-
-  """
+  return "Hello?"
 
 @app.route('/queues',	methods=['GET'])
 def	queues_index():
