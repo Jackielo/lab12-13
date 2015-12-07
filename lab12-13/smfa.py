79aa42625490c6e66b5e819370782922368e9e8f
Jackie Lo <j_lo@outlook.com>
2015-12-07 11:03:20 +0000

    Update

--- lab12-13/smfa.py
+++ lab12-13/smfa.py
@@ -13,7 +13,7 @@ def index():
   Create: curl	-X	POST	-H	'Content-Type:	application/json'	http://localhost:5000/queues	-d	'{"name":	"my-queue"}'
   Delete: curl	-X	DELETE	-H	'Accept:	application/json'	http://localhost:5000/queues/<mytestqueue>
   Get number of messages: curl	-X	GET	-H	'Accept:	application/json'	http://localhost:5000/queues/<mytestqueue>/msgs/count
-  Write a message: curl	-s	-X	POST	-H	'Accept:	application/json'	http://localhost:5000/queues/<mytestqueue>/msgs	-d	
+  Write a message: curl	-s	-X	POST	-H	'Accept:	application/json'	http://localhost:5000/queues/<mytestqueue>/msgs	-d
   '{"content":	"this	is	the	message	I	want	to	put	on	the	queue"}'
   Read a message: curl	-X	GET	-H	'Accept:	application/json'	http://localhost:5000/queues/<mytestqueue>/msgs
   Read and erase a message: curl	-X	DELETE	-H	'Accept:	application/json'	http://localhost:5000/queues/<mytestqueue>/msgs
@@ -31,6 +31,10 @@ def	queues_index():
     all.append(q.name)
   resp	=	json.dumps(all)
   return	Response(response=resp,	mimetype="application/json")
+
+def	get_conn():
+  key_id,	secret_access_key	=	urllib2.urlopen("http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key").read().split(':')
+  return	boto.sqs.connect_to_region("eu-west-1",	aws_access_key_id=key_id,	aws_secret_access_key=secret_access_key)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000, debug=True)
